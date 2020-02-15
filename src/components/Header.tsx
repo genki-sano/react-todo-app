da0eb0a97ec158474c3d25b3be4431ed2a6500b7
g-sano <g.sano.2441@gmail.com>
2020-02-15 23:31:57 +0900

    Headerコンポーネントの作成

---
+++ src/components/Header.tsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import styled from 'styled-components'
+import AppBar from '@material-ui/core/AppBar'
+import Toolbar from '@material-ui/core/Toolbar'
+import Typography from '@material-ui/core/Typography'
+import { Color } from '@/constants/color'
+
+const Warp = styled(AppBar)`
+  background-color: ${Color.blue.d};
+  background: linear-gradient(90deg, ${Color.blue.d}, ${Color.green.d});
+`
+
+const Header: React.FC = () => {
+  return (
+    <Warp position="static">
+      <Toolbar variant="dense">
+        <Typography variant="h6" color="inherit">
+          TodoApp
+        </Typography>
+      </Toolbar>
+    </Warp>
+  )
+}
+
+export default Header
