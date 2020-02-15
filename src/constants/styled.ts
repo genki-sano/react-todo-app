8906bfb30d7583ae7f5b974c34f3497cdd95d0d6
g-sano <g.sano.2441@gmail.com>
2020-02-15 18:48:12 +0900

    ベースカラーを定義

---
+++ src/constants/styled.ts
@@ -0,0 +1,93 @@
+export const Color = {
+  bulue: {
+    d: '#029df4',
+    l: '#d7effd',
+  },
+  bluegreen: {
+    d: '#0aaace',
+    l: '#d8f1f7',
+  },
+  green: {
+    d: '#11b7a8',
+    l: '#d9f3f1',
+  },
+  greenyellow: {
+    d: '#65c15e',
+    l: '#e6f5e5',
+  },
+  yellow: {
+    d: '#b9ca14',
+    l: '#f4f7d9',
+  },
+  yelloworange: {
+    d: '#dcb823',
+    l: '#f9f4dc',
+  },
+  orange: {
+    d: '#ffa531',
+    l: '#fff1de',
+  },
+  orangered: {
+    d: '#fa7c49',
+    l: '#feeae2',
+  },
+  red: {
+    d: '#f55361',
+    l: '#fde3e6',
+  },
+  redpurple: {
+    d: '#bc5f9a',
+    l: '#f4e5ef',
+  },
+  purple: {
+    d: '#826bd3',
+    l: '#ebe7f8',
+  },
+  purpleblue: {
+    d: '#4284e4',
+    l: '#e1ebfb',
+  },
+  gray: {
+    darker: '#121618',
+    dark: '#2d383c',
+    normal: '#45545b',
+    light: '#657a84',
+    lighter: '#98a9b0',
+    subtle: '#d0d6da',
+    subtler: '#e4e9ea',
+    subtlest: '#f2f3f5',
+  },
+}
+
+export const ButtonColor = {
+  default: {
+    darker: '#566870',
+    dark: Color.gray.light,
+    light: '#84959d',
+    lighter: '#d9dee1',
+  },
+  primary: {
+    darker: '#008ede',
+    dark: Color.bulue.d,
+    light: '#35b1f6',
+    lighter: Color.bulue.l,
+  },
+  info: {
+    darker: '#84cd7e',
+    dark: Color.greenyellow.d,
+    light: '#41bc38',
+    lighter: Color.greenyellow.l,
+  },
+  warning: {
+    darker: '#d1a908',
+    dark: Color.yelloworange.d,
+    light: '#e3c64f',
+    lighter: Color.yelloworange.l,
+  },
+  danger: {
+    darker: '#f77581',
+    dark: Color.red.d,
+    light: '#e7303f',
+    lighter: Color.red.l,
+  },
+}
