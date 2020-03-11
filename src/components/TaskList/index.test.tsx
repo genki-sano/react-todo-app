f29ddaa80af1a32170ed2bbc731fbe191cb6afbe
g-sano <g.sano.2441@gmail.com>
2020-03-11 21:16:19 +0900

    テストの追加

--- src/components/TaskList/index.test.tsx
+++ src/components/TaskList/index.test.tsx
@@ -2,10 +2,16 @@ import React from 'react'
 import { shallow } from 'enzyme'
 import TaskList from 'components/TaskList'
 import TaskItem from 'components/TaskItem'
+import { Task } from 'types/todos'
 
 describe('<TaskList />', () => {
   it('要素がない時に表示されない', () => {
     const wrapper = shallow(<TaskList tasks={[]} focus={false} />)
     expect(wrapper.find(TaskItem).exists()).toEqual(false)
+  })
+  it('要素がある時に表示される', () => {
+    const task: Task = { id: 0, text: 'テストを学ぶ', completed: false }
+    const wrapper = shallow(<TaskList tasks={[task]} focus={false} />)
+    expect(wrapper.find(TaskItem).exists()).toEqual(true)
   })
 })
